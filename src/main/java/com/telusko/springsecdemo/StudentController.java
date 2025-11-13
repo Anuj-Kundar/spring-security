cd25c204ba904b44ecc8e7e44532243915a8b20d
AnujKundar <anujkundar003@gmail.com>
2025-11-13 21:02:38 +0530

    add student class and controller

--- src/main/java/com/telusko/springsecdemo/StudentController.java
+++ src/main/java/com/telusko/springsecdemo/StudentController.java
@@ -1,4 +1,28 @@
 package com.telusko.springsecdemo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class StudentController {
+
+    List<Student>students=new ArrayList<>(List.of(
+            new Student(1,"Anuj","React"),
+            new Student(2,"Mythilesh","Java")
+    ));
+
+    @GetMapping("/students")
+    public List<Student>getStudents(){
+        return students;
+    }
+
+    @PostMapping("/students")
+    public void addStudent(@RequestBody Student student){
+        students.add(student);
+    }
 }
