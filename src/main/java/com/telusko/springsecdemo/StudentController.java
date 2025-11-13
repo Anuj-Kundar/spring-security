5c9e04ff5ae8a7f424937879db66094eca64aeda
AnujKundar <anujkundar003@gmail.com>
2025-11-13 21:12:43 +0530

    add getCsrfToken

--- src/main/java/com/telusko/springsecdemo/StudentController.java
+++ src/main/java/com/telusko/springsecdemo/StudentController.java
@@ -1,5 +1,7 @@
 package com.telusko.springsecdemo;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,11 @@ public class StudentController {
             new Student(1,"Anuj","React"),
             new Student(2,"Mythilesh","Java")
     ));
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
 
     @GetMapping("/students")
     public List<Student>getStudents(){
