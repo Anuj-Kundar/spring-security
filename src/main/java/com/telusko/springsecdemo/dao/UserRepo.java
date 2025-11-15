009219161739ff4d01037b4b3f98ac0ce9ed13fb
AnujKundar <anujkundar003@gmail.com>
2025-11-15 17:57:54 +0530

    add user repo

---
+++ src/main/java/com/telusko/springsecdemo/dao/UserRepo.java
@@ -0,0 +1,9 @@
+package com.telusko.springsecdemo.dao;
+
+import com.telusko.springsecdemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Integer>{
+    User findByUsername(String username);
+
+}
