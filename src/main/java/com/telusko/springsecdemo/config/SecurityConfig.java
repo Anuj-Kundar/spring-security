b2a8424751fee5cd5b471a01f2cfe1d63cc73421
AnujKundar <anujkundar003@gmail.com>
2025-11-29 22:37:35 +0530

    add jwt token and filter

--- src/main/java/com/telusko/springsecdemo/config/SecurityConfig.java
+++ src/main/java/com/telusko/springsecdemo/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +26,9 @@ public class SecurityConfig {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public AuthenticationProvider authProvider(UserDetailsService userDetailsService){
@@ -46,7 +50,8 @@ public class SecurityConfig {
                         .permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
--- src/main/java/com/telusko/springsecdemo/service/JwtService.java
+++ src/main/java/com/telusko/springsecdemo/service/JwtService.java
@@ -1,14 +1,47 @@
 package com.telusko.springsecdemo.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    //        HardCoded SecretKey
+    private static final String SECRET="TmV3U2VjcmV0S2V5Rm9ySldUU29uZ2luZ21hc2sgZmF2b3I=";
+
+    private String secretKey;
+
+    public JwtService(){
+        secretKey=generateSecretKey();
+    }
+
+//    Dynamic Key generation using method
+    public String generateSecretKey(){
+        try{
+            KeyGenerator keyGen=KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey=keyGen.generateKey();
+            System.out.println("Secret Key :"+secretKey.toString());
+            return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error generating secret key",e);
+        }
+    }
+
+
     public String generateToken(String username) {
 
         Map<String,Object>claims=new HashMap<>();
@@ -19,5 +52,10 @@ public class JwtService {
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis()+1000*60*3))
                 .signWith(getKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
